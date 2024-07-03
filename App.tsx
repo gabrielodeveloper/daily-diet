a2504a70222031be08dae2efe1024ecb46d66d39
gabrielodeveloper <gabriel0211oliveira15@gmail.com>
2024-07-02 23:19:06 -0300

    feat: Initial daily-diet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { 
+  useFonts, 
+  NunitoSans_400Regular, 
+  NunitoSans_700Bold 
+} from "@expo-google-fonts/nunito-sans";
+import { Home } from "@screens/Home";
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider  theme={theme}>
+      <StatusBar 
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent
+      />
+      <Home />
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/index.tsx
@@ -0,0 +1,34 @@
+export default {
+  COLORS: {
+      WHITE: '#FFFFFF',
+
+      GRAY_700: '#1B1D1E',
+      GRAY_600: '#333638',
+      GRAY_500: '#333638',
+      GRAY_400: '#B9BBBC',
+      GRAY_300: '#DDDEDF',
+      GRAY_200: '#EFF0F0',
+      GRAY_100: '#FAFAFA',
+
+      GREEN_DARK: '#639339',
+      GREEN_MID: '#CBE4B4',
+      GREEN_LIGHT: '#E5F0DB',
+  
+      RED_DARK: '##BF3B44',
+      RED_MID: '#F3BABD',
+      RED_LIGHT: '#F4E6E7',
+
+  },
+  FONT_FAMILY: {
+      REGULAR: 'NunitoSans_700Bold',
+      BOLD: 'NunitoSans_400Regular'
+    },
+  
+  FONT_SIZE: {
+      SM: 14,
+      MD: 16,
+      LG: 18,
+      XL: 24,
+      XXL: 32
+    }
+}
